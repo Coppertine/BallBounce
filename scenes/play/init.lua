2c75509928fd81424b8758ebf34b54439e306450
Coppertine <nick_bishop1@outlook.com>
2019-09-12 13:03:25 +1000

    Created Ball object

--- scenes/play/init.lua
+++ scenes/play/init.lua
@@ -7,7 +7,27 @@ local play = {
                 x = 0,
                 y = 0
             }
-            
         }
     }
-} 
+}
+
+function play:load()
+    loveframes.SetState("play")
+    
+end
+
+function play:keypressed(key) 
+    if key == "w"
+        ball.speedY -= 0.01
+    end
+    if key == "a" then
+        ball.speedX -= 0.01
+    end
+    if key == "s" then
+        ball.speedY += 0.01
+    end
+    if key == "d" then
+        ball.speedX += 0.01
+    end
+    
+end
