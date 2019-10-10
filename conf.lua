ec140c555fbe438b7d6baad3a042fa8158b2e5a3
Coppertine <37494321+Coppertine@users.noreply.github.com>
2019-10-10 15:20:40 +1000

    Set up Config for love

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(game)
+    game.identity = "ballbounce"
+    game.window.title = "BallBounce - Prototype"
+    game.window.highdpi = true
+    game.console = false
+    game.accelerometerjoystick = true
+    game.window.borderless = true
+    game.window.vsync = true
+end
