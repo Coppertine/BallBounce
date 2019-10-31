263c39353a7f33eea630370a834e11e9fc7fe16e
Coppertine <37494321+Coppertine@users.noreply.github.com>
2019-10-31 14:47:45 +1000

    Forgot some stuff

--- conf.lua
+++ conf.lua
@@ -6,6 +6,8 @@ function love.conf(game)
     game.accelerometerjoystick = true
     game.window.borderless = true
     game.window.vsync = true
+    game.window.width = 1920 / 2
+    game.window.height = 1080 / 2
 
     -- Modules not needed
     game.modules.video = false
--- main.lua
+++ main.lua
@@ -1,10 +1,13 @@
-loveframes = {}
 graphics = love.graphics
-states = {
-    menu = require("scenes/menu"),
-    play = require("scenes/play")
+game = {
+    current_state = "splash",
+    states = {
+        menu = require("scenes/menu"),
+        play = require("scenes/play"),
+        splash = require("scenes/splash")
+    }
 }
-
+json = require "lib/json"
 
 
 function toDecimalRGB(rgb)
@@ -15,12 +18,40 @@ function toDecimalRGB(rgb)
 end
 
 function toDecimalRGBA(rgba)
-    local r = rgb[1] / 255
-    local g = rgb[2] / 255
-    local b = rgb[3] / 255
+    local r = rgba[1] / 255
+    local g = rgba[2] / 255
+    local b = rgba[3] / 255
     return {r, g, b, a}
 end
 function resetColor()
     love.graphics.setColor(1,1,1,1)
 end
 
+
+function game:link_event(event)
+    love[event] = function(...)
+        if self.states[self.current_state] ~= nil then
+            if self.states[self.current_state][event] ~= nil then
+                self.states[self.current_state][event](self.states[self.current_state], ...)
+            end
+        end
+    end
+end
+
+game:link_event("draw")
+game:link_event("keypressed")
+game:link_event("update")
+
+function game:change_state(state)
+    if self.states[state] ~= nil then
+        if self.states[self.current_state].exited ~= nil then
+            self.states[self.current_state].exited(self.states[self.current_state])
+        end
+
+        self.current_state = state
+
+        if self.states[self.current_state].entered ~= nil then
+            self.states[self.current_state].entered(self.states[self.current_state])
+        end
+    end
+end
