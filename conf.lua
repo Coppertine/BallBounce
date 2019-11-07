028c7a1fa8b5b072e8da36ee2b29bbb878479627
Coppertine <37494321+Coppertine@users.noreply.github.com>
2019-11-07 10:35:01 +1000

    Fix the conf

--- conf.lua
+++ conf.lua
@@ -1,11 +1,10 @@
 function love.conf(game)
-    game.identity = "ballbounce"
     game.window.title = "BallBounce - Prototype"
     game.window.highdpi = true
     game.console = false
     game.accelerometerjoystick = true
-    game.window.borderless = true
-    game.window.vsync = true
+    game.window.borderless = false
+    game.window.vsync = false
     game.window.width = 1920 / 2
     game.window.height = 1080 / 2
 
